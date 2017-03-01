bec1a74881a3e8e352b6169a0b7fe7506fa2eb56
RWOverdijk <r.w.overdijk@gmail.com>
2017-03-01 14:38:50 +0100

    chore(project): commit files for part seven

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Wetland tutorial - _Inventory manager_
 This is the source code for the wetland tutorial, in which we build an inventory manager.
 
-This branch is part of step 6 of the tutorial: [**Lifecycle callbacks**](https://wetland.spoonx.org/Tutorial/lifecycle-callbacks.html).
+This branch is part of step 7 of the tutorial: [**EntityManager, Scope and UnitOfWork**](https://wetland.spoonx.org/Tutorial/entitymanager-scope-unitofwork.html).
 
 You can find the finished project [here](https://github.com/SpoonX/wetland-tutorial).
--- app/resource/category.js
+++ app/resource/category.js
@@ -1,5 +1,18 @@
-const express = require('express');
-const router  = express.Router();
+const express  = require('express');
+const router   = express.Router();
+const Category = require('../entity/Category');
+
+router.post('/', (req, res) => {
+  let wetland  = req.wetland;
+  let manager  = wetland.getManager();
+  let category = new Category;
+
+  category.name = req.body.name;
+
+  manager.persist(category).flush()
+    .then(() => res.json(category))
+    .catch(error => res.status(500).json({error}));
+});
 
 router.get('/', (req, res) => res.json({hello: 'from category.js'}));
 
