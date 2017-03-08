6867568b4bb5b9042e2147ab73d1bc97963d4adc
RWOverdijk <r.w.overdijk@gmail.com>
2017-03-08 18:27:31 +0100

    chore(project): add skeleton code for other adapter

--- wetland.js
+++ wetland.js
@@ -1,5 +1,17 @@
 const path = require('path');
 
 module.exports = {
-  entityPath: path.resolve(process.cwd(), 'app', 'entity')
+  entityPath: path.resolve(process.cwd(), 'app', 'entity'),
+  /* Template to configure your MySQL (or any other) client. Don't forget to install your client.
+   stores    : {
+     defaultStore: {
+       client    : 'mysql',
+       connection: {
+         host    : '127.0.0.1',
+         user    : 'spoonx',
+         database: 'wetland_tutorial'
+       }
+     }
+   }
+   */
 };
